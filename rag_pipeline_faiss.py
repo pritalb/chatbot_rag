dc15522aaf5758b3df0975cf2dfa8241e1c79bf7
pritalb <work.prital@gmail.com>
2025-01-20 22:40:06 +0530

    Implemented rag pipeline using faiss

---
+++ rag_pipeline_faiss.py
@@ -0,0 +1,65 @@
+from langchain.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.llms import huggingface_hub
+import pickle
+from langchain_community.vectorstores import FAISS
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+def create_rag_chain_faiss(model_name, index_path, docstore_path, hf_llm_id):
+    embeddings = HuggingFaceEmbeddings(model_name = model_name)
+
+    with open(index_path, 'rb') as f:
+        index = pickle.load(f)
+
+    with open(docstore_path, 'rb') as f:
+        docstore = pickle.load(f)
+
+    vectorstore = FAISS(
+        embedding_function = embeddings,
+        index = index,
+        docstore = docstore,
+        index_to_docstore_id = {i : str(i) for i in range(len(docstore._dict))}
+    )
+
+    prompt_template = """
+    You are a helpful customer support chatbot. Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you do not know, don't try to make up an answer.
+    Context:
+    {context}
+    """
+
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", prompt_template),
+            ("human", "{input}"),
+        ]
+    )
+
+    llm = huggingface_hub.HuggingFaceHub(
+        repo_id=hf_llm_id,
+        huggingfacehub_api_token = os.getenv("HUGGINGFACEHUB_API_TOKEN"),
+        model_kwargs={"temperature": 0, "max_length": 1024, "max_new_tokens": 250}
+    )
+
+    document_chain = create_stuff_documents_chain(llm, prompt)
+    chain = create_retrieval_chain(vectorstore.as_retriever(), document_chain)
+
+    return chain
+
+if __name__ == "__main__":
+    model_name = "sentence-transformers/all-mpnet-base-v2"
+    index_path = "./data/faiss_index.pkl"
+    docstore_path = "./data/faiss_docstore.pkl"
+    hf_llm_id = "google/flan-t5-small"
+    qa_chain = create_rag_chain_faiss(model_name, index_path, docstore_path, hf_llm_id)
+
+    question = "How can I save my password?"
+    response = qa_chain.invoke({"input": question})
+
+    print(f"Question: {question}")
+    print(f"Response: {response}")
--- vector_store.py
+++ vector_store.py
@@ -1,26 +1,30 @@
 import faiss
 import numpy as np
-import json
 from embedding_generator import generate_embeddings
 from preprocess import process_data
 import pickle
+from langchain_community.docstore.in_memory import InMemoryDocstore
+from langchain_core.documents import Document
 
-def create_and_populate_faiss_index(embeddings, index_path):
-    print("creating vector db...")
+def create_and_populate_faiss_index(embeddings, index_path, docstore_path):
     embeddings_array = np.array([item['embedding'] for item in embeddings]).astype('float32')    
     dimension = embeddings_array.shape[1]
+
     index = faiss.IndexFlatL2(dimension)
     index.add(embeddings_array)
-
     with open(index_path, 'wb') as f:
         pickle.dump(index, f)
 
-    metadata = [{
-        "question": item["question"],
-        "answer": item["answer"]
-    } for item in embeddings]
+    documents = {
+        str(i): Document(page_content=item["question"], metadata={"answer": item["answer"]}) for i, item in enumerate(embeddings)
+    }
 
-    return index, metadata
+    docstore = InMemoryDocstore()
+    docstore.add(texts = documents)
+    with open(docstore_path, 'wb') as f:
+        pickle.dump(docstore, f)
+
+    return index, docstore
 
 if __name__ == "__main__":
     print("running vector store....")
@@ -31,7 +35,8 @@ if __name__ == "__main__":
     embeddings = generate_embeddings(text_data, model_name)
 
     index_path = "./data/faiss_index.pkl"
-    index, metadata = create_and_populate_faiss_index(embeddings, index_path)
+    metadata_path = "./data/faiss_docstore.pkl"
+    index, docstore = create_and_populate_faiss_index(embeddings, index_path, metadata_path)
 
     print(f"Created and populated FAISS index at {index_path}")
-    print(f"metadata count:{len(metadata)}")
+    print(f"metadata count:{len(docstore._dict)}")
