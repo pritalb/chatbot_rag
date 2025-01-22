704ad39debe316386cc7dbf6d1124596cc346dcd
pritalb <work.prital@gmail.com>
2025-01-22 23:04:56 +0530

    implemented chatbot_response function in rag_pipeline_faiss.py

--- rag_pipeline_faiss.py
+++ rag_pipeline_faiss.py
@@ -54,15 +54,18 @@ def create_rag_chain_faiss(model_name, index_path, docstore_path, hf_llm_id):
 
     return chain
 
+def get_chatbot_response(question, model_name, index_path, docstore_path, hf_llm_id):
+    qa_chain = create_rag_chain_faiss(model_name, index_path, docstore_path, hf_llm_id)
+    return qa_chain.invoke({"input": question})
+
 if __name__ == "__main__":
     model_name = "sentence-transformers/all-mpnet-base-v2"
     index_path = "./data/faiss_index.pkl"
     docstore_path = "./data/faiss_docstore.pkl"
     hf_llm_id = "google/flan-t5-small"
-    qa_chain = create_rag_chain_faiss(model_name, index_path, docstore_path, hf_llm_id)
 
     question = "How can I save my password?"
-    response = qa_chain.invoke({"input": question})
+    response = get_chatbot_response(question, model_name, index_path, docstore_path, hf_llm_id)
 
     print(f"Question: {question}")
     print(f"Response: {response}")
