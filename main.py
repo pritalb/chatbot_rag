ec79f72663cb0c16e01b82c26a487c200f1e79a2
pritalb <work.prital@gmail.com>
2025-01-23 16:58:56 +0530

    Added logging to the chat api endpoint

--- main.py
+++ main.py
@@ -4,15 +4,19 @@ from rag_pipeline_faiss import get_chatbot_response
 from dotenv import load_dotenv
 import os
 import uvicorn
+import logging
 
 load_dotenv()
 app = FastAPI()
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 class Question(BaseModel):
     question: str
 
 @app.post("/chat/")
 async def chat_endpoint(question: Question):
+    logging.info(f"Received Question: {question.question}")
+
     model_name = "sentence-transformers/all-mpnet-base-v2"
     index_path = "./data/faiss_index.pkl"
     docstore_path = "./data/faiss_docstore.pkl"
@@ -20,6 +24,7 @@ async def chat_endpoint(question: Question):
 
     response = get_chatbot_response(question.question, model_name, index_path, docstore_path, hf_llm_id)
 
+    logging.info(f"Response: {response}")
     return {"response": response}
 
 if __name__ == "__main__":
