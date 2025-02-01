c4f6959e899f8b808840ec2ded052c724f3f3b63
pritalb <work.prital@gmail.com>
2025-02-01 23:00:02 +0530

    Reformatted code

--- main.py
+++ main.py
@@ -6,26 +6,29 @@ import os
 import json
 import logging
 import time
+import uvicorn
 from uuid import uuid4
 
 load_dotenv()
 app = FastAPI()
 
-# Configure basic logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 producer = KafkaProducer(
-    bootstrap_servers=os.getenv("KAFKA_HOST", "127.0.0.1") + ':9093',
-    value_serializer=lambda x: json.dumps(x).encode('utf-8')
-    )
-consumer = KafkaConsumer(
-    'llm_responses',
-    bootstrap_servers=os.getenv("KAFKA_HOST", "127.0.0.1") + ':9093',
-    auto_offset_reset='earliest',
-    enable_auto_commit=True,
-    group_id='api-consumer',
-    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+    bootstrap_servers=os.getenv("KAFKA_HOST", "127.0.0.1") + ":9093",
+    value_serializer=lambda x: json.dumps(x).encode("utf-8")
 )
+consumer = KafkaConsumer(
+    "llm_responses",
+    bootstrap_servers=os.getenv("KAFKA_HOST", "127.0.0.1") + ":9093",
+    auto_offset_reset="earliest",
+    enable_auto_commit=True,
+    group_id="api-consumer",
+    value_deserializer=lambda x: json.loads(x.decode("utf-8"))
+)
+
 
 class Question(BaseModel):
     question: str
@@ -37,15 +40,19 @@ async def chat_endpoint(question: Question):
         message_id = str(uuid4())
         logging.info(f"Received question: {question.question} id:{message_id}")
         data = {"message_id": message_id, "query": question.question}
-        producer.send('user_queries', value = data)
+        producer.send("user_queries", value = data)
         start_time = time.time()
         while True:
            for message in consumer:
-               if message.value['original_message']['message_id'] == message_id:
-                   logging.info(f"Response received for message id:{message_id} in {time.time() - start_time} seconds")
-                   return {"response": message.value["transformed_message"]}
+               if message.value["original_message"]["message_id"] == message_id:
+                    logging.info(
+                       f"Response received for message id:{message_id} in {time.time() - start_time} seconds"
+                    )
+                    return {"response": message.value["transformed_message"]}
                if time.time() - start_time > 30:
-                  raise HTTPException(status_code=504, detail="Timeout waiting for response from LLM")
+                    raise HTTPException(
+                        status_code=504, detail="Timeout waiting for response from LLM"
+                    )
 
 
     except Exception as e:
@@ -54,5 +61,4 @@ async def chat_endpoint(question: Question):
 
 
 if __name__ == "__main__":
-    import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
